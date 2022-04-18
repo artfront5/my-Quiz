bff653c94ea1efed53c65cf51c7dfeddcb2b7f9e
Artem Chaplygin <98418032+artfront5@users.noreply.github.com>
2022-04-18 19:26:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const button = document.querySelector('.open')
+button.addEventListener('click', chekTest)
+
+function chekTest() {
+  let result = 0;
+
+  let a1 = document.querySelector('.q1'); // вэлью, чтобы передать значение
+
+  if (a1.value === '4') {
+    result++
+  }
+
+  let a2 = document.querySelector('.q2') // вэлью, чтобы передать значение
+  if (a2.value === '6') {
+    result++
+  }
+  let name = 'Байкал'
+  let a3 = document.querySelector('.q3') // вэлью, чтобы передать значение
+
+  if (a3.value.toUpperCase() === name.toUpperCase()) {
+    result++
+  }
+
+  a1.value = '';
+  a2.value = '';
+  a3.value = '';
+
+  setTimeout(() => {
+    alert(`Количество правильных ответов: ${result}`)
+  }, 0);
+}
